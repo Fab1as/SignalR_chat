4359f48b7789fe14c9e704b606c3de5ce2ffba25
Fab1as <fabiasser@gmail.com>
2019-09-12 00:16:02 +0300

    +Initial commit : new clean project

---
+++ SignalRChat/DAL.Entities/ChatRoom.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace DAL.Entities
+{
+    public class ChatRoom : BaseEntity<Guid>
+    {
+        [Required]
+        public string Name { get; set; }
+    }
+}
